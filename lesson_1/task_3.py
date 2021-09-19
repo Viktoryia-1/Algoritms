38fe5759669df713bed819b93e5ec1e8d093f3bf
Viktoryia <sazanchukv@icloud.com>
2021-09-19 12:24:12 +0300

    Change task_3

--- lesson_1/task_3.py
+++ lesson_1/task_3.py
@@ -10,9 +10,16 @@
 
 import random
 import string
-random_int = random.randint(3, 20)
-random_r = random.uniform(-5, 5)
-random_letter = random.choices(string.ascii_letters[0:6])
-print(f"Случайное целое число от 3 до 20: {random_int} \n"
-      f"Случайное вещественное число от -5 до 5: {random_r} \n"
-      f"Случайная буква от 'a' до 'f': {random_letter}")
+d_for_int = input("Укажите диапазон для чисел через пробел: ")
+left, right = d_for_int.split(" ")
+left, right = int(left), int(right)
+d_for_letters = input("Укажите две буквы через пробел: ")
+letter_1, letter_2 = d_for_letters.split(" ")
+letter_1_ind = string.ascii_letters.index(letter_1)
+letter_2_ind = string.ascii_letters.index(letter_2)
+random_int = random.randint(abs(left), abs(right))
+random_r = random.uniform(left, right)
+random_letter = random.choices(string.ascii_letters[letter_1_ind:letter_2_ind])
+print(f"Случайное целое число от {abs(left)} до {abs(right)}: {random_int} \n"
+      f"Случайное вещественное число от {left} до {right}: {random_r} \n"
+      f"Случайная буква от {letter_1} до {letter_2}: {random_letter}")
